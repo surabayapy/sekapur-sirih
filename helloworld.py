29f3efcbc962eeb650ca976011cfa653c87a086f
Tegar Imansyah <egar.imansyah@gmail.com>
2018-02-03 12:43:06 +0700

    add hello world

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+# Untuk awal belajar menampilkan teks pada command line
+
+print("Hello World dengan petik dua")
+print('Hello World dengan petik satu')
+print('Setelah ini baris baru\nBaris baru')
+
+print() # Cetak Enter
+
+# Sekarang bermain dengan variabel
+a = 10 # Variabel dengan tipe data integer
+print('Variabel a = ', a) # Cetak teks dan variabel dalam satu file
+print('Variabel a = ', a, ' dengan tipe data ', type(a)) # Cetak teks, variabel dan tipe datanya
+
+a = 10.0 # Variabel dengan tipe data float
+print('Variabel a = ', a, ' dengan tipe data ', type(a)) # Cetak teks, variabel dan tipe datanya
+
+a = '10' # Variabel dengan tipe data string
+print('Variabel a = ', a, ' dengan tipe data ', type(a)) # Cetak teks, variabel dan tipe datanya
+
+a = True # Variabel dengan tipe data boolean
+print('Variabel a = ', a, ' dengan tipe data ', type(a)) # Cetak teks, variabel dan tipe datanya
+
+print() # Cetak Enter
